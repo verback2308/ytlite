ed9505ddd952e549b3dfe8441b88edc9816aeaee
yaoren8750 <144168162+yaoren8750@users.noreply.github.com>
2026-07-18 23:53:45 +0800

    更新 SearchViewController.swift

--- YTLite/Features/Search/SearchViewController.swift
+++ YTLite/Features/Search/SearchViewController.swift
@@ -48,7 +48,7 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Search"
+        title = "搜索"
         setupFiltersButton()
         setupSearchBar()
         setupTableView()
@@ -63,7 +63,7 @@ class SearchViewController: UIViewController {
 
     private func setupSearchBar() {
         searchBar.delegate = self
-        searchBar.placeholder = "Search YouTube"
+        searchBar.placeholder = "搜索YouTube"
         searchBar.text = lastQuery.isEmpty ? nil : lastQuery
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(searchBar)
